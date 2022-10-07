5b7dea231efa4e7ab2c8503e92c5edbe97452013
sch-28 <jansch2803@googlemail.com>
2022-10-07 14:39:28 +0200

    Added Bulma List Extension to display Wars

--- src/routes/dashboard/war/+page.svelte
+++ src/routes/dashboard/war/+page.svelte
@@ -4,12 +4,49 @@
 </script>
 
 <div class="wars">
-	{#each $manager.wars as war}
-		<div class="war">
-			<strong>{war.name}</strong>
-			{war.date}
-			<br />
-			Players: {war.players.length}
-		</div>
-	{/each}
+	<div class="list has-hoverable-list-items">
+		{#each $manager.wars as war}
+			<div class="list-item">
+				<div class="list-item-content">
+					<div class="list-item-title">{war.name} - {war.date}</div>
+					<div class="list-item-description">List item description</div>
+				</div>
+				<div class="list-item-controls">
+					<div class="buttons is-right">
+						<button class="button">
+							<span class="icon is-small">
+								<i class="fas fa-edit" />
+							</span>
+							<span>Edit</span>
+						</button>
+
+						<button class="button">
+							<span class="icon is-small">
+								<i class="fas fa-ellipsis-h" />
+							</span>
+						</button>
+					</div>
+				</div>
+			</div>
+		{/each}
+	</div>
 </div>
+
+<style>
+	.list {
+		color: #fff;
+		border-radius: var(--radius-base);
+	}
+
+	.list-item-title {
+		color: #fff;
+	}
+
+	.list-item {
+		cursor: pointer;
+	}
+
+	.list.has-hoverable-list-items .list-item:hover {
+		background-color: rgba(0, 0, 0, 0.1);
+	}
+</style>
